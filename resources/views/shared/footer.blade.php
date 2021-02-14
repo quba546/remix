56207e67627740769ce97953c1c4b18d727f0b7b
quba546 <quba546@wp.pl>
2021-02-14 13:44:39 +0100

    Mofify site map section in footer and social icons in footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,51 +1,48 @@
 <div class="row mx-auto custom-footer">
     <div class="col-12 col-xl-3 pt-3 pb-3 text-center">
-        <table class="vertical-center">
-            <thead>
-            <tr>
-                <td class="text-uppercase font-weight-bold letter-spacing-1">Regulaminy</td>
-                <td class="text-uppercase font-weight-bold letter-spacing-1 word-spacing-2">Mapa strony</td>
-                <td class="text-uppercase font-weight-bold letter-spacing-1 word-spacing-2">Klub w innych serwisach</td>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td><a href="#" class="font-white">Polityka prywatności</a></td>
-                <td><a href="#" class="font-white">Sekcja młodzieżowa</a></td>
-                <td><a href="#" target="_blank" class="font-white">90minut.pl</a></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><a href="#" class="font-white">O klubie</a></td>
-                <td><a href="#" target="_blank" class="font-white">laczynaspilka.pl</a></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><a href="#" class="font-white">Kontakt</a></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><a href="#" class="font-white">Terminarz</a></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><a href="#" class="font-white">Tabela</a></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><a href="#" class="font-white">Kadra</a></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><a href="#" class="font-white">Statystyki</a></td>
-                <td></td>
-            </tr>
-            </tbody>
-        </table>
+        <div class="container">
+            <div class="row">
+                <div class="col-4 d-flex">
+                    <ul class="list-inline mx-auto justify-content-center site-map-list">
+                        <li class="text-center text-uppercase font-weight-bold letter-spacing-1 word-spacing-2">Regulaminy</li>
+                    </ul>
+                </div>
+                <div class="col-4 d-flex">
+                    <ul class="list-inline mx-auto justify-content-center site-map-list site-map-list">
+                        <li class="text-uppercase font-weight-bold letter-spacing-1 word-spacing-2">Mapa strony</li>
+                    </ul>
+                </div>
+                <div class="col-4 d-flex">
+                    <ul class="list-inline mx-auto justify-content-center site-map-list site-map-list">
+                        <li class="text-uppercase font-weight-bold letter-spacing-1 word-spacing-2">Klub w innych serwisach</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 d-flex">
+                    <ul class="list-inline mx-auto justify-content-center site-map-list site-map-list">
+                        <li><a href="#" class="font-white">Polityka prywatności</a></li>
+                    </ul>
+                </div>
+                <div class="col-4 d-flex">
+                    <ul class="list-inline mx-auto justify-content-center site-map-list site-map-list">
+                        <li><a href="#" class="font-white">Sekcja młodzieżowa</a></li>
+                        <li><a href="#" class="font-white">O klubie</a></li>
+                        <li><a href="#" class="font-white">Kontakt</a></li>
+                        <li><a href="#" class="font-white">Terminarz</a></li>
+                        <li><a href="#" class="font-white">Tabela</a></li>
+                        <li><a href="#" class="font-white">Kadra</a></li>
+                        <li><a href="#" class="font-white">Statystyki</a></li>
+                    </ul>
+                </div>
+                <div class="col-4 d-flex">
+                    <ul class="list-inline mx-auto justify-content-center site-map-list site-map-list">
+                        <li><a href="#" target="_blank" class="font-white">90minut.pl</a></li>
+                        <li><a href="#" target="_blank" class="font-white">laczynaspilka.pl</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="col-xl-1">
         <hr class="d-none d-xl-flex vertical-hr">
@@ -55,21 +52,18 @@
         <span class="text-uppercase letter-spacing-1 word-spacing-2 font-weight-bold">Odwiedź nas w mediach społecznościowych</span>
         <br>
         <span class="social-icons">
-                    <a href="#" target="_blank">
-                        <i class="fab fa-facebook mr-3"></i>
-                    </a>
-                    <a href="#" target="_blank">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                </span>
-        <br>
-        <span class="text-uppercase letter-spacing-1 word-spacing-2">
-                    Tel:<a href="tel:123123123" target="_blank" class="font-white"> 123123123</a>
-                </span>
-        <br>
-        <span class="text-uppercase letter-spacing-1 word-spacing-2">
-            E-mail:<a href="mailto:example@domain.com" target="_blank" class="font-white"> example@domain.com</a>
+            <a href="#" target="_blank">
+                <i class="fab fa-facebook mr-3"></i>
+            </a>
+            <a href="#" target="_blank">
+                <i class="fab fa-instagram"></i>
+            </a>
         </span>
+        <br>
+        <br>
+        <span class="text-uppercase letter-spacing-1 word-spacing-2">Tel:<a href="tel:123123123" target="_blank" class="font-white"> 123123123</a></span>
+        <br>
+        <span class="text-uppercase letter-spacing-1 word-spacing-2">E-mail:<a href="mailto:example@domain.com" target="_blank" class="font-white"> example@domain.com</a></span>
         <br>
         <span class="text-uppercase letter-spacing-1 word-spacing-2">NIP: 6871791491</span>
         <br>
