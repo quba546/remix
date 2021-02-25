1c67ff2ee6fea192b669f9ed1c40936778fc396a
quba546 <quba546@wp.pl>
2021-02-25 17:29:19 +0100

    Fix displaying players images

--- resources/views/user/season/player-details.blade.php
+++ resources/views/user/season/player-details.blade.php
@@ -9,48 +9,48 @@
                     <hr>
                     <div class="text-justify mt-5">
                         <div class="row">
-                            <div class="col-12 col-lg-6">
-                                <div class="player-img t">
-                                    <img src="{{ $player->image ? asset('storage/' . $player->image) : asset('/storage/blank-profile-picture.png') }}" alt="Zdjęcie gracza">
+                            <div class="col-12 col-lg-6 text-center">
+                                <div class="player-profile-img">
+                                    <img src="{{ $player->image ? asset('storage/' . $player->image) : asset('/storage/blank-profile-picture.png') }}" class=" border border-success" alt="Zdjęcie gracza">
                                 </div>
                             </div>
-                            <div class="col-12 col-lg-6 border border-success rounded">
+                            <div class="col-12 col-lg-6 mt-4 mt-lg-0 border border-success rounded p-4">
                                 <table>
                                     <tr>
-                                        <td>Nazwisko:</td>
-                                        <td><strong class="text-uppercase">{{ $player->last_name ?? '' }}</strong></td>
+                                        <td class="font-16">Nazwisko:</td>
+                                        <td class="font-20"><strong class="text-uppercase">{{ $player->last_name ?? '' }}</strong></td>
                                     </tr>
                                     <tr>
-                                        <td>Imię:</td>
-                                        <td><strong>{{ $player->first_name ?? '' }}</strong></td>
+                                        <td class="font-16">Imię:</td>
+                                        <td class="font-16"><strong>{{ $player->first_name ?? '' }}</strong></td>
                                     </tr>
                                     <tr>
-                                        <td>Nr:</td>
-                                        <td><strong>{{ $player->nr ?? '' }}</strong></td>
+                                        <td class="font-16">Nr:</td>
+                                        <td class="font-16"><strong>{{ $player->nr ?? '' }}</strong></td>
                                     </tr>
                                     <tr>
-                                        <td>Pozycja:</td>
-                                        <td><strong>{{ $player->position ?? '' }}</strong></td>
+                                        <td class="font-16">Pozycja:</td>
+                                        <td class="font-16"><strong>{{ $player->position ?? '' }}</strong></td>
                                     </tr>
                                     <tr>
-                                        <td>Bramki:</td>
-                                        <td><strong>{{ $player->goals ?? '' }}</strong></td>
+                                        <td class="font-16">Bramki:</td>
+                                        <td class="font-16"><strong>{{ $player->goals ?? '' }}</strong></td>
                                     </tr>
                                     <tr>
-                                        <td>Asysty:</td>
-                                        <td><strong>{{ $player->assists ?? '' }}</strong></td>
+                                        <td class="font-16">Asysty:</td>
+                                        <td class="font-16"><strong>{{ $player->assists ?? '' }}</strong></td>
                                     </tr>
                                     <tr>
-                                        <td>Czyste konta:</td>
-                                        <td><strong>{{ $player->clean_sheets ?? '' }}</strong></td>
+                                        <td class="font-16">Czyste konta:</td>
+                                        <td class="font-16"><strong>{{ $player->clean_sheets ?? '' }}</strong></td>
                                     </tr>
                                     <tr>
-                                        <td>Żółte kartki:</td>
-                                        <td><strong>{{ $player->yellow_cards ?? '' }}</strong></td>
+                                        <td class="font-16">Żółte kartki:</td>
+                                        <td class="font-16"><strong>{{ $player->yellow_cards ?? '' }}</strong></td>
                                     </tr>
                                     <tr>
-                                        <td class="pr-4">Czerwone kartki:</td>
-                                        <td><strong>{{ $player->red_cards ?? '' }}</strong></td>
+                                        <td class="pr-4 font-16">Czerwone kartki:</td>
+                                        <td class="font-16"><strong>{{ $player->red_cards ?? '' }}</strong></td>
                                     </tr>
                                 </table>
                             </div>
