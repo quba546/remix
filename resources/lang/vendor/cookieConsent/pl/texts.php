0d7549851e5d82a21c90de7aeade2daf2ff186e9
quba546 <quba546@wp.pl>
2021-02-26 23:11:22 +0100

    Update dependencies, few changes in views (override bootstrap success color by own)

--- resources/lang/vendor/cookieConsent/pl/texts.php
+++ resources/lang/vendor/cookieConsent/pl/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'Twoje doświadczenia na tej witrynie będą lepsze dzięki cookies.',
-    'agree' => 'Zezwalaj na cookie',
+    'message' => 'Twoje doświadczenia na tej witrynie będą lepsze dzięki cookies. Jeśli chcesz dowiedzieć się więcej odwiedź stronę z polityką prywatności. Jeśli nie zezwalasz na zapisywanie pliki cookie opuść stronę.',
+    'agree' => 'Zezwalam',
 ];
