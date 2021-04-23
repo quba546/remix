c70f799694ef21ccf38e1372b2dc50da16e06f26
quba546 <quba546@wp.pl>
2021-04-23 15:26:56 +0200

    Comment unused controller

--- app/CustomClasses/UploadPhoto.php
+++ app/CustomClasses/UploadPhoto.php
@@ -11,65 +11,65 @@ use Illuminate\Support\Facades\Storage;
 
 class UploadPhoto
 {
-    const TEMPORARY_PATH = 'public/tmp/';
-
-    public function saveTemporaryUploadedImage(UploadedFile $uploadedFile): string
-    {
-        $file = $uploadedFile;
-        $folder = uniqid() . '-' . now()->timestamp;
-        $filename = Storage::putFile(self::TEMPORARY_PATH . $folder, $file); // add image to storage
-        $filename = str_replace(self::TEMPORARY_PATH . $folder . '/', '', $filename);
-
-        TemporaryFile::create([
-            'folder' => $folder,
-            'filename' => $filename
-        ]);
-
-        return $folder;
-    }
-
-    /** @noinspection PhpUnhandledExceptionInspection */
-    public function savePlayerImage(string $playerImage, ?string $oldImagePath): ?string
-    {
-        // delete garbage form request
-        $folder = substr($playerImage, 0, strpos($playerImage, '<'));
-        $temporaryFile = TemporaryFile::where('folder', '=', $folder)->first();
-
-        if ($temporaryFile) {
-            Storage::move(
-                'public/tmp/' . $folder . '/' . $temporaryFile->filename,
-                'public/players-images/' . $temporaryFile->filename
-            );
-            Storage::deleteDirectory('public/tmp/' . $folder);
-            $temporaryFile->delete();
-
-            if (isset($oldImagePath)) {
-                $oldPlayerImagePath = $oldImagePath;
-                $oldPlayerImage = str_replace('players-images/', '', $oldPlayerImagePath);
-                // delete old player image from storage
-                Storage::delete('public/players-images/' . $oldPlayerImage);
-            }
-        }
-
-        return $temporaryFile->filename ?? null;
-    }
-
-    /** @noinspection PhpUnhandledExceptionInspection */
-    public function savePhotoToGallery(string $uploadedPhotoFilename): ?string
-    {
-        // delete garbage form request
-        $folder = substr($uploadedPhotoFilename, 0, strpos($uploadedPhotoFilename, '<'));
-        $temporaryFile = TemporaryFile::where('folder', '=', $folder)->first();
-
-        if ($temporaryFile) {
-            Storage::move(
-                'public/tmp/' . $folder . '/' . $temporaryFile->filename,
-                'public/photos/' . $temporaryFile->filename
-            );
-            Storage::deleteDirectory('public/tmp/' . $folder);
-            $temporaryFile->delete();
-        }
-
-        return $temporaryFile->filename ?? null;
-    }
+//    private const TEMPORARY_PATH = 'public/tmp/';
+//
+//    public function saveTemporaryUploadedImage(UploadedFile $uploadedFile): string
+//    {
+//        $file = $uploadedFile;
+//        $folder = uniqid() . '-' . now()->timestamp;
+//        $filename = Storage::putFile(self::TEMPORARY_PATH . $folder, $file); // add image to storage
+//        $filename = str_replace(self::TEMPORARY_PATH . $folder . '/', '', $filename);
+//
+//        TemporaryFile::create([
+//            'folder' => $folder,
+//            'filename' => $filename
+//        ]);
+//
+//        return $folder;
+//    }
+//
+//    /** @noinspection PhpUnhandledExceptionInspection */
+//    public function savePlayerImage(string $playerImage, ?string $oldImagePath): ?string
+//    {
+//        // delete garbage form request
+//        $folder = substr($playerImage, 0, strpos($playerImage, '<'));
+//        $temporaryFile = TemporaryFile::where('folder', '=', $folder)->first();
+//
+//        if ($temporaryFile) {
+//            Storage::move(
+//                'public/tmp/' . $folder . '/' . $temporaryFile->filename,
+//                'public/players-images/' . $temporaryFile->filename
+//            );
+//            Storage::deleteDirectory('public/tmp/' . $folder);
+//            $temporaryFile->delete();
+//
+//            if (isset($oldImagePath)) {
+//                $oldPlayerImagePath = $oldImagePath;
+//                $oldPlayerImage = str_replace('players-images/', '', $oldPlayerImagePath);
+//                // delete old player image from storage
+//                Storage::delete('public/players-images/' . $oldPlayerImage);
+//            }
+//        }
+//
+//        return $temporaryFile->filename ?? null;
+//    }
+//
+//    /** @noinspection PhpUnhandledExceptionInspection */
+//    public function savePhotoToGallery(string $uploadedPhotoFilename): ?string
+//    {
+//        // delete garbage form request
+//        $folder = substr($uploadedPhotoFilename, 0, strpos($uploadedPhotoFilename, '<'));
+//        $temporaryFile = TemporaryFile::where('folder', '=', $folder)->first();
+//
+//        if ($temporaryFile) {
+//            Storage::move(
+//                'public/tmp/' . $folder . '/' . $temporaryFile->filename,
+//                'public/photos/' . $temporaryFile->filename
+//            );
+//            Storage::deleteDirectory('public/tmp/' . $folder);
+//            $temporaryFile->delete();
+//        }
+//
+//        return $temporaryFile->filename ?? null;
+//    }
 }
